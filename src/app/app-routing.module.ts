f9a25710766b65f52665ba786464294582620b5d
re-vc <frederikrehovic@gmail.com>
2023-02-24 14:09:45 +0100

    +nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'details', component: DetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
